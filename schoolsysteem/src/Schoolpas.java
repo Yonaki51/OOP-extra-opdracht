251b20e11a3c0f0b7c0172588522ca4dd6d419f2
Yonaki51 <yonivandon@hotmail.com>
2026-03-26 13:33:36 +0100

    java bestanden aangemaakt

---
+++ schoolsysteem/src/Schoolpas.java
@@ -0,0 +1,8 @@
+public class Schoolpas {
+    private String ID;
+    private String naam;
+    private double saldo;
+    private boolean geldig;
+    private boolean heeftLiftToegang;
+    private boolean isDocent;
+}
