211e5fd12a57c08150d8806017a353c598b469df
Yonaki51 <yonivandon@hotmail.com>
2026-03-26 13:42:47 +0100

    schoolpas class gevuld met benodigde variabelen en methodes

--- schoolsysteem/src/Schoolpas.java
+++ schoolsysteem/src/Schoolpas.java
@@ -1,8 +1,40 @@
 public class Schoolpas {
+    private static int volgendID = 1;
+
     private String ID;
     private String naam;
     private double saldo;
     private boolean geldig;
     private boolean heeftLiftToegang;
     private boolean isDocent;
+
+    public Schoolpas(String naam, double saldo, boolean geldig, boolean heeftLiftToegang, boolean isDocent) {
+        this.ID = "PAS-" + volgendID;
+        volgendID++;
+        this.naam = naam;
+        this.saldo = saldo;
+        this.geldig = geldig;
+        this.heeftLiftToegang = heeftLiftToegang;
+        this.isDocent = isDocent;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isDocent() {
+        return isDocent;
+    }
+
+    public boolean isGeldig() {
+        return geldig;
+    }
+
+    public boolean heeftLiftToegang() {
+        return heeftLiftToegang;
+    }
 }
