30ced758239709263c6b638300b87afe977067db
Yonaki51 <yonivandon@hotmail.com>
2026-03-27 10:43:19 +0100

    main aan hget opzetten

---
+++ schoolsysteem/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Schoolpas leerling = new Schoolpas("Jan", 5, true, true, false);
+        Deurlezer deurlezer = new Deurlezer();
+        Koffiezetapparaat koffie = new Koffiezetapparaat(1);
+        Printer printer = new Printer(0.60, 0.60);
+        App app = new App();
+        
+
+    }
+}
