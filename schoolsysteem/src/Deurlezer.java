8a63f9365fdb32cf84fcbe4531dc0fbc94565df2
Yonaki51 <yonivandon@hotmail.com>
2026-03-27 09:01:56 +0100

    methode gemaakt om een lift te openen en om een deur/garagedeur te openen

--- schoolsysteem/src/Deurlezer.java
+++ schoolsysteem/src/Deurlezer.java
@@ -2,11 +2,20 @@ public class Deurlezer {
     public void openDeur(Schoolpas pas) {
         if (!pas.isGeldig()) {
             System.out.println("pas is niet geldig.");
-        }
-        if (!pas.isDocent()) {
+        } else if (!pas.isDocent()) {
             System.out.println("je hebt geen toegang.");
         } else {
-            System.out.println("de deur gaat open");
+            System.out.println("de deur gaat open.");
+        }
+    }
+
+    public void openLift(Schoolpas pas) {
+        if (!pas.isGeldig()) {
+            System.out.println("pas is niet geldig.");
+        } else if (!pas.heeftLiftToegang()) {
+            System.out.println("je hebt geen toegang tot de lift.");
+        } else {
+            System.out.println("de lift gaat open.");
         }
     }
 }
