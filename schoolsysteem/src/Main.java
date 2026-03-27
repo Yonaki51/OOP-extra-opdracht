164de4a57e045f09da0ac13543f048d9b663c68f
Yonaki51 <yonivandon@hotmail.com>
2026-03-27 13:35:59 +0100

    tests geprobeerd in de main

--- schoolsysteem/src/Main.java
+++ schoolsysteem/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Schoolpas leerling = new Schoolpas("Jan", 5, true, true, false);
+        Schoolpas docent = new Schoolpas("Peter", 0, true, true, true);
         Deurlezer deurlezer = new Deurlezer();
         Koffiezetapparaat koffie = new Koffiezetapparaat(1);
         Printer printer = new Printer(0.60, 0.60);
         App app = new App();
         
+        deurlezer.openDeur(leerling);
+        deurlezer.openDeur(docent);
+        koffie.koffieZetten(docent);
 
     }
 }
